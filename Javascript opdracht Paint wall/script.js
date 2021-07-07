53ce719ca8a411dd080100c733826e8215bf4112
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-07 17:02:43 +0200

    Add files via upload

---
+++ Javascript opdracht Paint wall/script.js
@@ -0,0 +1,13 @@
+const paintWall = function (brush, text) {
+    console.log ("paint wall red");
+};
+
+paintWall (brush, "green");
+paintWall (brush, "blue");
+
+const wallChoice = function (side, text) {
+
+};
+
+wallChoice (side, "north wall");
+
