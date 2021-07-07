44b5fefa3ad68246ee888060abb0379a22c0f6c0
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-07 18:06:33 +0200

    Add files via upload

---
+++ Function writing style/script.js
@@ -0,0 +1,24 @@
+function berekening1 (number) {
+    console.log ("number1, number2");
+    console.log ("number1 * number1");
+    console.log ("number2 * number2");
+    console.log ("number1 * number1 + number2 * number2");
+    return;
+}
+
+const berekening2 = function (number) {
+    console.log ("number1, number2");
+    console.log ("number1 * number1");
+    console.log ("number2 * number2");
+    console.log ("number1 * number1 + number2 * number2");
+    return;
+};
+
+
+const berekening3 = (number) => {
+    console.log ("number1, number2");
+    console.log ("number1 * number1");
+    console.log ("number2 * number2");
+    console.log ("number1 * number1 + number2 * number2");
+    return;
+};
