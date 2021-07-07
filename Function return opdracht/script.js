73d64421489a995b43530c5eeb116d232af399f6
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-07 17:42:12 +0200

    Add files via upload

---
+++ Function return opdracht/script.js
@@ -0,0 +1,22 @@
+const checkNumber = function (number) {
+    console.log ("Checking number");
+    return >100 = true;
+};
+
+console.log ("return value of the funcion")
+
+const bouncerBrenda = function (number) {
+    console.log ("come on in");
+    console.log ("it's too busy now, come back later");
+    console.log ("this is a club for adults");
+    return bouncerBrenda;
+};
+
+console.log (50, 100, 19);
+
+
+const calculateAverage = function (number) {
+    console.log ("calculate average of five numbers");
+    Math.round (average number)
+    return;
+};
