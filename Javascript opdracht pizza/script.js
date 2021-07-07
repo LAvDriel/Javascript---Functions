f9f01eba6bb6f8013e418cd2a30c3c0e84ec2832
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-07 14:27:03 +0200

    Add files via upload

---
+++ Javascript opdracht pizza/script.js
@@ -0,0 +1,25 @@
+const makePizza = function () {
+    console.log ("Make dough");
+    console.log ("Make bottom");
+    console.log ("Put on topping");
+}
+
+const name = Linda 
+const age = 37
+makePizza ()
+
+const makeSushi = function () {
+    console.log ("Cook the rice");
+    console.log ("Cut the ingredients");
+    console.log ("Roll sushi");
+}
+
+makeSushi ()
+
+// Order
+makeSushi ()
+makeSushi ()
+makeSushi ()
+makePizza ()
+makePizza ()
+
