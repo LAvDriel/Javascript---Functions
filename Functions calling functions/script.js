393a17de8ee18bf7120b0f85b99ad68883a864e0
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-07 19:30:05 +0200

    Add files via upload

---
+++ Functions calling functions/script.js
@@ -0,0 +1,14 @@
+const checkGuest = function (age) {
+        if (age >= 18) {respons = "true"
+    
+        } else {respons = "false"
+    
+};
+};
+
+const checkSenior = function (age) {
+   console.log ("Hello there")
+   console.log ("hey kiddo")
+};
+};
+
