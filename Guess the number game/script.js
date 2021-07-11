59ce2a2022e033d0230b78761f133cf1ca3c3d3f
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-11 17:19:19 +0200

    Add files via upload

---
+++ Guess the number game/script.js
@@ -0,0 +1,21 @@
+console.log ("Welkom! Wat is je naam?")
+
+function (text) {
+    prompt ("Welkom! Wat is je naam?");
+}
+
+function (text) {
+    alert ("Hey name");
+}
+
+function (number) {
+    prompt ("Give a number >= 0 and <= 25?");
+    Math.random 
+}
+
+function (number) {
+    alert ("The number is wrong");
+    prompt ("Want to guess again");
+}
+
+
