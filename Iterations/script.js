515f529088d02b354c8ee05ea1eb2e0164b90be6
LAvDriel <84100361+LAvDriel@users.noreply.github.com>
2021-07-11 17:00:46 +0200

    Add files via upload

---
+++ Iterations/script.js
@@ -0,0 +1,25 @@
+let color = {
+    color: yellow,
+    color: blue,
+    color: orange,
+    color: red,
+};
+
+person.color = yellow, blue, orange, red;
+console.log (person.color)
+
+while (color) {
+    color++
+    return color
+}
+
+
+for (color = green; color = yellow, blue, orange, red; color++) {
+    Array.length 
+}
+
+
+
+for each (color) {
+    Array.prototype.forEach(color)
+}
